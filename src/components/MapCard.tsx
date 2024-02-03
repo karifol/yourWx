4246ae48fe7c4d8358d883fd3961c34a63f179ac
karifol <karigolhorinouchi@gmail.com>
2024-02-03 23:27:35 +0900

    dev

--- src/components/MapCard.tsx
+++ src/components/MapCard.tsx
@@ -1,7 +1,47 @@
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 import { View, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+
+type Amedas = {
+  [key: string]: {
+    id: string
+    name: string
+    lat: number[]
+    lon: number[]
+    type: string
+  }
+}
 
 const MapComponent = (): JSX.Element => {
+  const [zoomLevel, setZoomLevel] = useState(10)
+  const [amedasData, setAmedasData] = useState([])
+
+  const handleRegionChange = (region: any): void => {
+    const newZoomLevel = Math.round(Math.log(360 / region.longitudeDelta) / Math.LN2)
+    setZoomLevel(newZoomLevel)
+  }
+
+  useEffect(() => {
+    const fetchAmedas = async () => {
+      const url = 'https://www.jma.go.jp/bosai/amedas/const/amedastable.json'
+      const data: Amedas = await fetch(url)
+        .then(async (response) => await response.json())
+      const amedasArr = []
+      for (const key in data) {
+        const type = data[key].type
+        if (type === 'A') {
+          amedasArr.push(data[key])
+        }
+      }
+      setAmedasData(amedasArr)
+    }
+    fetchAmedas()
+  }, [])
+
+  const handlePress = (e: any): void => {
+    console.log('ddd')
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -12,7 +52,21 @@ const MapComponent = (): JSX.Element => {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
         }}
-      />
+        onRegionChangeComplete={handleRegionChange}
+      >
+        {
+          amedasData.map((amedas: any) => (
+            <Marker
+              coordinate={{
+                latitude: amedas.lat[0] + amedas.lat[1] / 60,
+                longitude: amedas.lon[0] + amedas.lon[1] / 60
+              }}
+              onPress={handlePress}
+              key = {amedas.id}
+            />
+          ))
+        }
+      </MapView>
     </View>
   )
 }
