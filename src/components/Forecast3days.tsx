c48f71ae996a1588670f1ba4ce0fb6f1618584d5
karifol <karigolhorinouchi@gmail.com>
2024-02-08 21:44:48 +0900

    dev

--- src/components/Forecast3days.tsx
+++ src/components/Forecast3days.tsx
@@ -1,9 +1,12 @@
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native'
 import { useEffect, useState } from 'react'
-import telops from './weatherCode'
+import _telops from './weatherCode'
+
 interface Props {
   prefecture: string
 }
+
+const telops = _telops as Record<number, string[]>
 
 const Forecast3days = (props: Props): JSX.Element => {
   const { prefecture } = props
@@ -12,6 +15,7 @@ const Forecast3days = (props: Props): JSX.Element => {
   const [forecast, setForecast] = useState<any>({})
   const [areaArr, setAreaArr] = useState<any[]>([])
   const [timeArr, setTimeArr] = useState<string[]>(['', '', ''])
+
   useEffect(() => {
     const fetchTable = async (): Promise<void> => {
       const url = 'https://www.jma.go.jp/bosai/common/const/area.json'
@@ -42,7 +46,6 @@ const Forecast3days = (props: Props): JSX.Element => {
       const obj = {} as any
       data[0].timeSeries[0].areas.forEach((item: any) => {
         obj[item.area.name] = item
-        console.log(item.weatherCodes)
       })
       setForecast(obj[area] as any || {})
 
@@ -78,6 +81,39 @@ const Forecast3days = (props: Props): JSX.Element => {
     setArea(areaArr[0])
   } , [areaArr])
 
+  const imgObj = {
+    100: require('../wx/100.png.png'),
+    101: require('../wx/101.png.png'),
+    102: require('../wx/102.png.png'),
+    104: require('../wx/104.png.png'),
+    110: require('../wx/110.png.png'),
+    112: require('../wx/112.png.png'),
+    115: require('../wx/115.png.png'),
+    200: require('../wx/200.png.png'),
+    201: require('../wx/201.png.png'),
+    202: require('../wx/202.png.png'),
+    204: require('../wx/204.png.png'),
+    210: require('../wx/210.png.png'),
+    212: require('../wx/212.png.png'),
+    215: require('../wx/215.png.png'),
+    300: require('../wx/300.png.png'),
+    301: require('../wx/301.png.png'),
+    302: require('../wx/302.png.png'),
+    303: require('../wx/303.png.png'),
+    308: require('../wx/308.png.png'),
+    311: require('../wx/311.png.png'),
+    313: require('../wx/313.png.png'),
+    314: require('../wx/314.png.png'),
+    400: require('../wx/400.png.png'),
+    401: require('../wx/401.png.png'),
+    402: require('../wx/402.png.png'),
+    403: require('../wx/403.png.png'),
+    406: require('../wx/406.png.png'),
+    411: require('../wx/411.png.png'),
+    413: require('../wx/413.png.png'),
+    414: require('../wx/414.png.png')
+  } as Record<number, any>
+
   const handlePress = (area: string): void => {
     setArea(area)
   }
@@ -111,8 +147,8 @@ const Forecast3days = (props: Props): JSX.Element => {
                       </View>
                       <View style={styles.icon}>
                         <Image
-                          source={{ uri: `https://www.jma.go.jp/bosai/forecast/img/${100}.png` }}
                           style={{ width: 100, height: 100 }}
+                          source={imgObj[forecast.weatherCodes[index]]}
                         />
                       </View>
                       <View style={styles.pops}>
@@ -203,7 +239,7 @@ const styles = StyleSheet.create({
     borderBottomColor: '#efefef',
     borderBottomWidth: 1,
     borderTopColor: '#efefef',
-    borderTopWidth: 1
+    borderTopWidth: 1,
   },
   pops: {
     width: '100%',
